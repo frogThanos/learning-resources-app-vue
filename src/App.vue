6b241439b727fbea5f61b3c23f502f8f6305c66c
frogThanos <kthanoss90@gmail.com>
2021-01-29 13:50:00 +0100

    create list item component and add some basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <ul></ul>
+  <ul>
+    <LearningResourceItem
+    v-for="res in storedResources"
+    :key="res.id"
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+    ></LearningResourceItem>
+  </ul>
 </template>
 
 <script>
+import LearningResourceItem from './components/LearningResource/LearningResourceItem';
 
 export default {
   name: 'App',
+  components: {
+    LearningResourceItem
+  },
   data() {
     return {
       storedResources: [{
-
+        id: 'official-guide',
+        title: 'Official Guide',
+        description: 'The official Vue.js documentation.',
+        link: 'https://vuejs.org/',
+      },
+      {
+        id: 'google',
+        title: 'Google',
+        description: 'The official Google website.',
+        link: 'https://google.com/',
       }]
     }
   }
@@ -17,12 +38,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
 }
 </style>

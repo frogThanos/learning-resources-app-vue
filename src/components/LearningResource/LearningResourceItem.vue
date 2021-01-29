c79def4d562bab1e89f493540b5eed32fc0ddc52
frogThanos <kthanoss90@gmail.com>
2021-01-29 14:33:13 +0100

    add to global

--- src/components/LearningResource/LearningResourceItem.vue
+++ src/components/LearningResource/LearningResourceItem.vue
@@ -14,12 +14,8 @@
 </template>
 
 <script>
-import BaseCard from "../UI/BaseCard";
 export default {
   props: ["title", "description", "link"],
-  components: {
-    BaseCard,
-  },
 };
 </script>
 
